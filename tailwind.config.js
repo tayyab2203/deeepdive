2e81f6e350adfd3ac04d849dbaf7dd71a659cc84
Tayyab Sajjad <tayyabsajjad4679@gmail.com>
2025-08-11 18:09:14 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // ✅ Make sure .jsx is included
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
